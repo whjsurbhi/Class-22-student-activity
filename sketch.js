97b2a5a31980e1290bacbf4a61a594ac5dcceeff
whjtest <70915301+whjtest@users.noreply.github.com>
2022-04-06 17:40:21 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,34 @@
+//namespacing (optional)
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+
+var engine, world;
+
+function setup() {
+  createCanvas(400,400);
+  engine = Engine.create();
+  world = engine.world;
+
+  var options = {
+
+    isStatic : true
+
+  }
+
+  object = Bodies.rectangle(200,100,50,50, options);
+  World.add(world, object);
+
+
+}
+
+function draw() {
+  background(0); 
+  Engine.update(engine);
+
+  rectMode(CENTER)
+  rect(object.position.x, object.position.y, 50,50);
+
+  
+  drawSprites();
+}
